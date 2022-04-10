81f4e4161e885c8bc340d3f9d90b1eeaa92e7a7a
Miguelcj1 <miguelluffy7@gmail.com>
2022-04-10 15:49:37 +0100

    Testes

--- codigo_fonte/CasaInteligente.java
+++ codigo_fonte/CasaInteligente.java
@@ -1,17 +1,17 @@
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
+
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
- * A CasaInteligente faz a gestão dos SmartDevices que existem e dos
+ * A CasaInteligente faz a gestão dos SmartDevices que existem e dos 
  * espaços (as salas) que existem na casa.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
 public class CasaInteligente {
-
+   
     private String morada;
     private Map<String, SmartDevice> devices; // identificador -> SmartDevice
     private Map<String, List<String>> locations; // Espaço -> Lista codigo dos devices
@@ -41,3 +41,99 @@ public class CasaInteligente {
         this.devices = ci.getMapDevices();
         this.locations = ci.getMapLocations();
     }
+
+    /**
+     * (ADDED) Devolve um map com os devices.
+     */
+    private Map<String, SmartDevice> getMapDevices() {
+        Map<String, SmartDevice> r = new HashMap<>();
+        for (String k : this.devices.keySet()){
+            r.put(k, this.devices.get(k).clone());
+        }
+        return r;
+    }
+
+    /**
+     * (ADDED) Devolve uma lista com os devices duma determinada sala.
+     */
+    private List<String> getListDevices(String chave){
+        List r = this.locations.get(chave).stream().collect(Collectors.toList());
+        return r;
+    }
+
+    /**
+     * (ADDED) Devolve um map com as locations.
+     */
+    private Map<String, List<String>> getMapLocations() {
+        Map<String, List<String>> r = new HashMap<>();
+        for (String k : this.locations.keySet()){
+            r.put(k, this.locations.get(k).stream().collect(Collectors.toList()));
+        }
+        return r;
+    }
+
+    /**
+     * (ADDED) Devolve a morada da casa.
+     */
+    public String getMorada(){
+        return this.morada;
+    }
+    
+    public void setDeviceOn(String devCode) {
+        this.devices.get(devCode).turnOn();
+    }
+    
+    public boolean existsDevice(String id) {return this.devices.containsKey(id);}
+    
+    public void addDevice(SmartDevice s) {
+        this.devices.put(s.getID(), s.clone());
+    }
+
+    public SmartDevice getDevice(String s) {
+        if (this.devices.containsKey(s)) {
+            return this.devices.get(s).clone();
+        }
+        else return null;
+    }
+    
+    public void setOn(String s, boolean b) {
+        this.devices.get(s).setOn(b);
+    }
+    
+    public void setAllOn(boolean b) {
+        for(String s : this.devices.keySet()){
+            setOn(s, b);
+        }
+    }
+    
+    public void addRoom(String s) {
+        List<String> l = new ArrayList<String>();
+        this.locations.put(s, l);
+    }
+    
+    public boolean hasRoom(String s) {
+        return this.locations.containsKey(s);
+    }
+    
+    public void addToRoom (String s1, String s2) {
+        this.locations.get(s1).add(s2);
+    }
+    
+    public boolean roomHasDevice (String s1, String s2) {
+        return this.locations.get(s1).contains(s2);
+    }
+
+    // (ADDED)
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CasaInteligente ci = (CasaInteligente) o;
+        return ci.morada.equals(morada) && ci.devices.equals(devices) && ci.locations.equals(locations);
+    }
+
+    // (ADDED)
+    public CasaInteligente clone(){
+        return new CasaInteligente(this);
+    }
+}
---
+++ codigo_fonte/SmartSpeaker.java
@@ -0,0 +1,99 @@
+
+/*********************************************************************************/
+/** DISCLAIMER: Este código foi criado e alterado durante as aulas práticas      */
+/** de POO. Representa uma solução em construção, com base na matéria leccionada */ 
+/** até ao momento da sua elaboração, e resulta da discussão e experimentação    */
+/** durante as aulas. Como tal, não deverá ser visto como uma solução canónica,  */
+/** ou mesmo acabada. É disponibilizado para auxiliar o processo de estudo.      */
+/** Os alunos são encorajados a testar adequadamente o código fornecido e a      */
+/** procurar soluções alternativas, à medida que forem adquirindo mais           */
+/** conhecimentos de POO.                                                        */
+/*********************************************************************************/
+
+/**
+ * Um SmartSpeaker é um SmartDevice que além de ligar e desligar permite também
+ * reproduzir som.
+ * Consegue ligar-se a um canal (por simplificação uma rádio online) e permite
+ * a regulação do seu nível de volume.
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class SmartSpeaker extends SmartDevice {
+    public static final int MAX = 20; //volume máximo
+    
+    private int volume;
+    private String channel;
+
+
+    /**
+     * Constructor for objects of class SmartSpeaker
+     */
+    public SmartSpeaker() {
+        super(); // chama o construtor da superclasse
+        // initialise instance variables
+        this.volume = 0;
+        this.channel = "";
+    }
+
+    public SmartSpeaker(String s) {
+        super(s); // chama o construtor da superclasse com o determinado parametro.
+        // initialise instance variables
+        this.volume = 10;
+        this.channel = "";
+    }
+
+
+    public SmartSpeaker(String cod, String channel, int i) {
+        // initialise instance variables
+        super(cod); // chama o construtor da superclasse com o determinado parametro.
+        this.channel = channel;
+        if (i >= 0 && i <= MAX){
+            this.volume = i;
+        }
+        else {
+            this.volume = 0;
+        }
+    }
+
+
+    public SmartSpeaker(SmartSpeaker ss){
+        super(ss);
+        this.volume = ss.getVolume();
+        this.channel = ss.getChannel();
+    }
+
+
+    public void volumeUp() {
+        if (this.volume<MAX) this.volume++;
+    }
+
+    public void volumeDown() {
+        if (this.volume>0) this.volume--;
+    }
+
+    public int getVolume() {
+        return this.volume;
+    }
+    
+    public String getChannel() {return this.channel;}
+
+    public void setChannel(String c) {
+        this.channel = c;
+    }
+
+    public boolean equals (Object o){
+        if (this == o) {
+            return true;
+        }
+        if (o == null || o.getClass() != this.getClass()){
+            return false;
+        }
+        SmartSpeaker s = (SmartSpeaker) o;
+        return (super.equals(o) && this.volume == s.getVolume() && this.channel == s.getChannel());
+    }
+
+    @Override
+    public SmartDevice clone() {
+        return new SmartSpeaker(this);
+    }
+}
