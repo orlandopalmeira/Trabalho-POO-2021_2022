2760414cead5a3267f6be910f120025a3a3912d2
Katilho <pemicama12@gmail.com>
2022-04-11 13:24:54 +0100

    small corrections and tests

--- testes/SmartCameraTest.java
+++ testes/SmartCameraTest.java
@@ -40,10 +40,10 @@ public class SmartCameraTest {
 
     @Test
     public void testGetSizeOfFIle() {
-        SmartCamera smartCam1 = new SmartCamera("c1", 2160, 1600, 5);
-        assertEquals(5, smartCam1.getSizeOfFile());
+        SmartCamera smartCam1 = new SmartCamera("c1", 2160, 1600, 100);
+        assertEquals(100, smartCam1.getSizeOfFile());
         SmartCamera smartCam2 = new SmartCamera(smartCam1);
-        assertEquals(5, smartCam2.getSizeOfFile());
+        assertEquals(100, smartCam2.getSizeOfFile());
         smartCam1 = new SmartCamera("c1");
         assertEquals(0, smartCam1.getSizeOfFile());
         smartCam1 = new SmartCamera();
@@ -53,11 +53,11 @@ public class SmartCameraTest {
     @Test
     public void testSetSizeOfFIle() {
         SmartCamera smartCam1 = new SmartCamera("c1");
-        smartCam1.setSizeOfFile(10);
-        assertEquals(10, smartCam1.getSizeOfFile());
-        smartCam1.setSizeOfFile(3);
-        smartCam1.setSizeOfFile(8);
-        assertEquals(8, smartCam1.getSizeOfFile());
+        smartCam1.setSizeOfFile(100);
+        assertEquals(100, smartCam1.getSizeOfFile());
+        smartCam1.setSizeOfFile(30);
+        smartCam1.setSizeOfFile(80);
+        assertEquals(80, smartCam1.getSizeOfFile());
     }
     
 }
