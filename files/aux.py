faef296895bb4412c5b3c0614065bb4e8d70551d
orlandopalmeira <orlandopalmeira51@gmail.com>
2022-04-14 17:32:12 +0100

    Melhorias nos ficheiros e script de teste

--- files/aux.py
+++ files/aux.py
@@ -10,13 +10,15 @@ houses = open("./files/houses_test.txt","w")
 # People 
 # Name(string);NIF(int)
 bools = ["true","false"]
+brands = ["Sony","Samsung","Xiaomi","Apple","Asus","LG","Acer","OnePlus"]
+length_b = len(brands)
 # generate devices
 # SmartBulb;ID(string);ON/OFF(bool);TONE(int);DIMENSION(double)
 # SmartSpeaker;ID(string);ON/OFF(bool);Volume(int);Channel(string);Marca(string)
 # SmartCamera;ID(string);ON/OFF(bool);resx(int);resy(int);sizeoffile(float)
 for i in range(1,101):
     devices.write("SmartBulb;b{0};{1};1;10\n".format(i,bools[i%2]))
-    devices.write("SmartSpeaker;s{0};{1};10;Channel{0};Brand{0}\n".format(i,bools[(i+1)%2]))
+    devices.write("SmartSpeaker;s{0};{1};10;Channel{0};{2}\n".format(i,bools[(i+1)%2],brands[i%length_b]))
     devices.write("SmartCamera;c{0};{1};1080;1920;100\n".format(i,bools[i%2]))
 
 # generate people
@@ -25,7 +27,7 @@ for i in range(1,21):
     people.write("Person{0};{0}\n".format(i))
 
 providers = ["EDP","Endesa","GoldEnergy","Iberdrola","Galp"]
-length = len(providers)
+length_p = len(providers)
 # generate houses
 # Morada(string);[(Room:[d1.d2.d2])];NIF(int);Fornecedor(string)
 for i in range(1,21):
@@ -39,7 +41,7 @@ for i in range(1,21):
         houses.write("b{0}.s{0}.c{0}.".format(j))
     houses.seek(houses.tell()-1,0);
     houses.write("])];")
-    houses.write("{0};{1}\n".format(i,providers[i%length]))
+    houses.write("{0};{1}\n".format(i,providers[i%length_p]))
 
 devices.close()
 people.close()
